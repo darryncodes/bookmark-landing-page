358368dfd13b4cbdcd342f85019a845fbbaf5936
darryncodes <darryncodes@gmail.com>
2024-04-22 19:45:37 +0100

    feature - creating main call to action, e-mail input including client-side validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
-import './app.css';
 import Container from './components/UI/Container/Container';
 import Navigation from './components/Navigation/Navigation';
 import Hero from './components/Hero/Hero';
 import Tabs from './components/Tabs/Tabs';
 import Cards from './components/Cards/Cards';
 import Faqs from './components/Faqs/Faqs';
+import Contact from './components/Contact/Contact';
 
 const App = () => {
     return (
-        <Container>
-            <Navigation />
-            <Hero />
-            <Tabs />
-            <Cards />
-            <Faqs />
-        </Container>
+        <>
+            <Container>
+                <Navigation />
+                <Hero />
+                <Tabs />
+                <Cards />
+                <Faqs />
+            </Container>
+            <Contact />
+        </>
     );
 };
 
---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,84 @@
+import { useState } from 'react';
+import Centralised from './../UI/Centralised/Centralised';
+import styles from './Contact.module.css';
+import errorIcon from './../../assets/icon-error.svg';
+
+const Contact = () => {
+    const [input, setInput] = useState('');
+    const [message, setMessage] = useState('');
+    const [error, setError] = useState(false);
+
+    function handleInput(e) {
+        if (input.length) {
+            setMessage('');
+            setError(false);
+        }
+
+        setInput(e.target.value);
+    }
+
+    function validateEmail(email) {
+        var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+        return reg.test(email);
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        if (input.trim().length === 0) {
+            setMessage(`Don't forget to enter an email`);
+            setError(true);
+            return;
+        }
+
+        if (validateEmail(input) === false) {
+            setMessage(`Whoops, make sure it's an email`);
+            setError(true);
+            return;
+        }
+
+        setInput('');
+    }
+
+    return (
+        <section className={styles.contact}>
+            <div className={styles.container}>
+                <Centralised>
+                    <p className={styles.content}>35,000+ already joined</p>
+                    <h2 className={styles.heading}>
+                        Stay up-to-date with what we’re doing
+                    </h2>
+                    <form className={styles.form} onSubmit={handleSubmit}>
+                        <div className={styles.group}>
+                            <label htmlFor='email'></label>
+                            <input
+                                type='text'
+                                id='email'
+                                placeholder='Enter your email address'
+                                value={input.toLowerCase()}
+                                onChange={handleInput}
+                                className={`${styles.input} ${
+                                    error ? styles.error : null
+                                }`}
+                            />
+
+                            {error && (
+                                <img src={errorIcon} className={styles.icon} />
+                            )}
+                            {message && (
+                                <span className={styles.message}>
+                                    {message}
+                                </span>
+                            )}
+                        </div>
+                        <button type='submit' className={styles.button}>
+                            Contact us
+                        </button>
+                    </form>
+                </Centralised>
+            </div>
+        </section>
+    );
+};
+
+export default Contact;
